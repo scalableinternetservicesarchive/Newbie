a5ba1c9623dc77ad669159def62254fc0f20782c
Tielong Su <tielongs@gmail.com>
2015-04-24 14:42:25 -0700

    fixed paperclip bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
     has_many :comments
     has_attached_file :post_image, :styles => { :thumb => "100x100>"}
+    validates_attachment_content_type :post_image, :content_type => ["image/jpg",
+        "image/jpeg", "image/png", "image/gif"]
 end
