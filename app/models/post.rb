67f91e03291f8823c14b0438b31e419bf8e9b843
Tielong Su <tielongs@gmail.com>
2015-05-05 22:20:49 -0700

    change avatar size && minor tweaking of gui

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
     has_many :comments
-    has_attached_file :post_image, :styles => { :thumb => "100x100>"}
+    has_attached_file :post_image, :styles => { :thumb => "100x100>", :medium => "300x300>"}
     validates_attachment_content_type :post_image, :content_type => ["image/jpg",
                                                                      "image/jpeg", "image/png", "image/gif"]
 
