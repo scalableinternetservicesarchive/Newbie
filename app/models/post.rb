44b56c8fcea33151d1be038e99d8a32fe73c7654
Qiuhan Ding <dingqiuhan@gmail.com>
2015-05-08 14:36:02 -0700

    Add Geocoder

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  #before_save :default_values
     has_many :comments
     has_many :votes
     has_attached_file :post_image, :styles => { :thumb => "100x100>", :medium => "300x300>"}
@@ -7,5 +8,14 @@ class Post < ActiveRecord::Base
 
     acts_as_taggable
 
+    geocoded_by :ipaddress,
+    :latitude => :latitude, :longitude => :longitude
+    after_validation :geocode
+
     after_save ThinkingSphinx::RealTime.callback_for(:post)
+
+    #def default_values
+      #self.downvote_number = 0
+      #self.upvote_number = 0
+    #end
 end
