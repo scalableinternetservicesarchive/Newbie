f0f3160361df5c2bd87c01db27be5b1c540af950
ogodcjm <bianpengyuan@gmail.com>
2015-05-31 19:38:25 -0700

    change session forget settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   before_filter :get_unread
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   def get_unread
   	@unread = 0
