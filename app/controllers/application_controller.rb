a8a1ff00cb3daacd07d6761fb6522735d20b7300
ogodcjm <bianpengyuan@gmail.com>
2015-06-01 10:59:17 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  skip_before_filter :verify_authenticity_token
   before_filter :get_unread
 
   def get_unread
