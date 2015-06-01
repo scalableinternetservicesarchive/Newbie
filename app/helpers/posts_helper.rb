714286a489ee4bde62b485125c9eb78ecb851657
Tielong Su <tielongs@gmail.com>
2015-05-31 18:16:24 -0700

    server side caching

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,6 @@
 module PostsHelper
     include ActsAsTaggableOn::TagsHelper
+    def cache_key_for_post(post)
+        "post-#{post.id}-#{post.updated_at}-#{post.pictures.size}-#{post.upvote_number}-#{post.downvote_number}-#{post.favorite_posts.size}"
+    end
 end
