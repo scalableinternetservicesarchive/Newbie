73012fef3e7ee94aaaf66cc682d22e67f35ea8a4
Qiuhan Ding <dingqiuhan@gmail.com>
2015-06-01 21:25:11 -0700

    client side caching

--- app/controllers/favorite_post_controller.rb
+++ app/controllers/favorite_post_controller.rb
@@ -1,5 +1,7 @@
 class FavoritePostController < ApplicationController
   def show
-    @posts = current_user.favorites.paginate(:page =>params[:page], :per_page => 5).eager_load(:comments,:favorite_posts,:pictures)
+    if stale?([Comment.all, FavoritePost.all, current_user, current_user.favorites, alert, notice])
+      @posts = current_user.favorites.paginate(:page =>params[:page], :per_page => 5).eager_load(:comments,:favorite_posts,:pictures)
+    end
   end
 end
