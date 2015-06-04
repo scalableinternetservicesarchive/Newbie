573a6ea39701d9d1ef1a987011345ad45682a10b
Qiuhan Ding <dingqiuhan@gmail.com>
2015-06-04 15:29:10 -0700

    modify server cache

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
     include ActsAsTaggableOn::TagsHelper
     def cache_key_for_post(post)
-        "post-#{post.id}-#{post.updated_at}-#{post.pictures.size}-#{post.upvote_number}-#{post.downvote_number}-#{post.favorite_posts.size}"
+        "post-#{post.id}-#{post.updated_at}-#{post.pictures.size}-#{post.upvote_number}-#{post.downvote_number}-#{post.favorite_posts.size}-#{post.comments.size}"
     end
 end
